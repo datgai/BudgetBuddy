baa4553ee1af7cb3045e14e4fab4dfefadbd9411
jiaxuan10 <limjiaxuan1357@gmail.com>
2024-03-20 22:43:20 +0800

    Add 'News' Page

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,181 @@
+<!-- Container for demo purpose -->
+<div class="container my-24 md:px-6 p-5">
+  <!-- Section: Design Block -->
+  <section class="mb-15 md:text-left">
+    <h2 class="mb-12 text-3xl font-bold">News</h2>
+
+    <div class="relative mb-6">
+      <div class="mb-6 ml-auto w-full shrink-0 grow-0 basis-auto">
+        <div
+          class="mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+          data-te-ripple-init
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/standard/city/018.jpg"
+            class="w-full"
+            alt="Louvre"
+          />
+        </div>
+      </div>
+
+      <div class="absolute top-24 left-7 text-2xl text-white text-wrap pr-3">
+        Crypto investors should be prepared to lose all their money, BOE
+        governor says
+      </div>
+
+      <div class="absolute bottom-3 left-7 text-xs text-white text-wrap pr-5">
+        “I’m going to say this very bluntly again,” he added. “Buy them only if
+        you’re prepared to lose all your money.”
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="info">
+      <div class="flex justify-between text-base">
+        <div class="font-semibold">Latest</div>
+        <div>See all</div>
+      </div>
+      <div class="flex justify-between text-base my-2">
+        <div class="font-semibold underline underline-offset-8">All</div>
+        <div>Trading</div>
+        <div>Crypto</div>
+        <div>Banking</div>
+        <div>Real Estate</div>
+      </div>
+    </div>
+
+    <!-- Container for demo purpose -->
+    <div class="@apply h-[50px] flex flex-col gap-y-5 mt-5">
+      <!-- Section: Design Block -->
+      <!-- Comment -->
+      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
+        <img
+          class="w-[120px] rounded-[15px]"
+          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
+          alt=""
+        />
+        <div class="detail">
+          <div class="h-4/5">
+            <div class="text-[gray] text-[15px]">Malaysia</div>
+            <div class="title">
+              Bank Negara committed to ensure Malaysia's financial system
+              remains resilient
+            </div>
+          </div>
+          <div class="flex items-center justify-between mt-0.5">
+            <div class="flex items-center gap-x-2.5">
+              <img
+                class="w-[25px] h-[25px] rounded-[50%]"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
+                alt=""
+              />
+              <div class="name">BBC</div>
+            </div>
+            <div class="time">
+              <i class="fa-solid fa-clock"></i>
+              <p class="clock-time">10 min ago</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
+        <img
+          class="w-[120px] rounded-[15px]"
+          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
+          alt=""
+        />
+        <div class="detail">
+          <div class="h-4/5">
+            <div class="text-[gray] text-[15px]">Malaysia</div>
+            <div class="title">
+              Bank Negara committed to ensure Malaysia's financial system
+              remains resilient
+            </div>
+          </div>
+          <div class="flex items-center justify-between mt-0.5">
+            <div class="flex items-center gap-x-2.5">
+              <img
+                class="w-[25px] h-[25px] rounded-[50%]"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
+                alt=""
+              />
+              <div class="name">BBC</div>
+            </div>
+            <div class="time">
+              <i class="fa-solid fa-clock"></i>
+              <p class="clock-time">10 min ago</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
+        <img
+          class="w-[120px] rounded-[15px]"
+          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
+          alt=""
+        />
+        <div class="detail">
+          <div class="h-4/5">
+            <div class="text-[gray] text-[15px]">Malaysia</div>
+            <div class="title">
+              Bank Negara committed to ensure Malaysia's financial system
+              remains resilient
+            </div>
+          </div>
+          <div class="flex items-center justify-between mt-0.5">
+            <div class="flex items-center gap-x-2.5">
+              <img
+                class="w-[25px] h-[25px] rounded-[50%]"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
+                alt=""
+              />
+              <div class="name">BBC</div>
+            </div>
+            <div class="time">
+              <i class="fa-solid fa-clock"></i>
+              <p class="clock-time">10 min ago</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
+        <img
+          class="w-[120px] rounded-[15px]"
+          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
+          alt=""
+        />
+        <div class="detail">
+          <div class="h-4/5">
+            <div class="text-[gray] text-[15px]">Malaysia</div>
+            <div class="title">
+              Bank Negara committed to ensure Malaysia's financial system
+              remains resilient
+            </div>
+          </div>
+          <div class="flex items-center justify-between mt-0.5">
+            <div class="flex items-center gap-x-2.5">
+              <img
+                class="w-[25px] h-[25px] rounded-[50%]"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
+                alt=""
+              />
+              <div class="name">BBC</div>
+            </div>
+            <div class="time">
+              <i class="fa-solid fa-clock"></i>
+              <p class="clock-time">10 min ago</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section: Design Block -->
+  </section>
+  <!-- Container for demo purpose -->
+</div>
+
+<!-- Container for demo purpose -->
+
+<style>
+</style>
