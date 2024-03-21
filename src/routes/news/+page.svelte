6b3f8fca44fc2c01e197eafa58944707c14099fa
Alex Chee <xxchee555xx@gmail.com>
2024-03-21 17:33:45 +0800

    :rocket: IMPROVE : created NewsItem components

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import NewsItem from "../NewsItem.svelte";
+</script>
+
 <!-- Container for demo purpose -->
 <div class="container my-24 md:px-6 p-5">
   <!-- Section: Design Block -->
@@ -44,138 +48,19 @@
       </div>
     </div>
 
-    <!-- Container for demo purpose -->
     <div class="@apply h-[50px] flex flex-col gap-y-5 mt-5">
-      <!-- Section: Design Block -->
-      <!-- Comment -->
-      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
-        <img
-          class="w-[120px] rounded-[15px]"
-          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
-          alt=""
-        />
-        <div class="detail">
-          <div class="h-4/5">
-            <div class="text-[gray] text-[15px]">Malaysia</div>
-            <div class="title">
-              Bank Negara committed to ensure Malaysia's financial system
-              remains resilient
-            </div>
-          </div>
-          <div class="flex items-center justify-between mt-0.5">
-            <div class="flex items-center gap-x-2.5">
-              <img
-                class="w-[25px] h-[25px] rounded-[50%]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
-                alt=""
-              />
-              <div class="name">BBC</div>
-            </div>
-            <div class="time">
-              <i class="fa-solid fa-clock"></i>
-              <p class="clock-time">10 min ago</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
-        <img
-          class="w-[120px] rounded-[15px]"
-          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
-          alt=""
-        />
-        <div class="detail">
-          <div class="h-4/5">
-            <div class="text-[gray] text-[15px]">Malaysia</div>
-            <div class="title">
-              Bank Negara committed to ensure Malaysia's financial system
-              remains resilient
-            </div>
-          </div>
-          <div class="flex items-center justify-between mt-0.5">
-            <div class="flex items-center gap-x-2.5">
-              <img
-                class="w-[25px] h-[25px] rounded-[50%]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
-                alt=""
-              />
-              <div class="name">BBC</div>
-            </div>
-            <div class="time">
-              <i class="fa-solid fa-clock"></i>
-              <p class="clock-time">10 min ago</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
-        <img
-          class="w-[120px] rounded-[15px]"
-          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
-          alt=""
-        />
-        <div class="detail">
-          <div class="h-4/5">
-            <div class="text-[gray] text-[15px]">Malaysia</div>
-            <div class="title">
-              Bank Negara committed to ensure Malaysia's financial system
-              remains resilient
-            </div>
-          </div>
-          <div class="flex items-center justify-between mt-0.5">
-            <div class="flex items-center gap-x-2.5">
-              <img
-                class="w-[25px] h-[25px] rounded-[50%]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
-                alt=""
-              />
-              <div class="name">BBC</div>
-            </div>
-            <div class="time">
-              <i class="fa-solid fa-clock"></i>
-              <p class="clock-time">10 min ago</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="@apply flex gap-x-[15px] pb-2.5 rounded-[30px]">
-        <img
-          class="w-[120px] rounded-[15px]"
-          src="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
-          alt=""
-        />
-        <div class="detail">
-          <div class="h-4/5">
-            <div class="text-[gray] text-[15px]">Malaysia</div>
-            <div class="title">
-              Bank Negara committed to ensure Malaysia's financial system
-              remains resilient
-            </div>
-          </div>
-          <div class="flex items-center justify-between mt-0.5">
-            <div class="flex items-center gap-x-2.5">
-              <img
-                class="w-[25px] h-[25px] rounded-[50%]"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
-                alt=""
-              />
-              <div class="name">BBC</div>
-            </div>
-            <div class="time">
-              <i class="fa-solid fa-clock"></i>
-              <p class="clock-time">10 min ago</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <NewsItem
+        itemThumbnail="https://www.investopedia.com/thmb/W2u8B017D-GDqZxH-ddc8BLS8qo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/top_6_apps_for_financial_news-5bfc3459c9e77c0026b6a2bf.jpg"
+        itemCountry="Malaysia"
+        itemTitle="Bank Negara committed to ensure Malaysia's financial system remains
+        resilient"
+        authorThumbnail="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/BBC_News_logo.svg/2560px-BBC_News_logo.svg.png"
+        authorTitle="BBC"
+        authoredAgo="10 min ago"
+      ></NewsItem>
     </div>
-
-    <!-- Section: Design Block -->
   </section>
-  <!-- Container for demo purpose -->
 </div>
-
-<!-- Container for demo purpose -->
 
 <style>
 </style>
