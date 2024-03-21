2c5f8b19c34bee8d4a52920ebdfa64d903af4cce
Alex Chee <xxchee555xx@gmail.com>
2024-03-21 16:05:03 +0800

    :new: NEW : add taskItem component

---
+++ src/routes/TaskItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let taskMission: String;
+  export let isCompleted: String;
+  export let experiencePoints: String;
+</script>
+
+<div class="flex bg-transparent backdrop-blur-lg rounded-full w-full px-2 py-2">
+  <div class="flex-1 w-[60%] flex-row my-auto">
+    <p class="font-bold text-lg text-[var(--color-text)] truncate">
+      {taskMission}
+    </p>
+    <p class="truncate text-[var(--color-text-inactive)]">{isCompleted}</p>
+  </div>
+  <div class="flex m-auto"><h3>{experiencePoints} XP</h3></div>
+</div>
