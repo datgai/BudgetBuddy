418f4620f8612d9a0ad8ad1da9155283ab2c42b1
Alex Chee <xxchee555xx@gmail.com>
2024-03-24 11:27:10 +0800

    :rocket: IMPROVE : logo navigates to home

--- src/lib/components/MobileHeader.svelte
+++ src/lib/components/MobileHeader.svelte
@@ -4,6 +4,8 @@
 </script>
 
 <header class="flex md:hidden">
-  <div class="flex-1"><img src={Logo} height="50" width="50" alt="" /></div>
+  <div class="flex-1">
+    <a href="/"><img src={Logo} height="50" width="50" alt="" /></a>
+  </div>
   <Profile />
 </header>
