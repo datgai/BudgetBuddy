1c50bb2ee6a0839d18ab66eb8fd58970fe6e6019
Alex Chee <xxchee555xx@gmail.com>
2024-04-04 23:30:18 +0800

    :new: NEW : add register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-  <title>Login</title>
+  <title>Register</title>
 </svelte:head>
 
 <div>
@@ -11,7 +11,7 @@
     <img src={Logo} width="300" alt="Logo" class="mx-auto my-8" />
   </div>
   <div class="flex flex-col mx-2">
-    <h1 class="font-bold text-4xl my-1 py-4">Sign In</h1>
+    <h1 class="font-bold text-4xl my-1 py-4">Sign Up</h1>
     <form action="POST">
       <div class="flex flex-col mb-8">
         <div class="my-1">
@@ -32,30 +32,12 @@
             class="mx-auto my-2 p-4 w-[100%] rounded-lg bg-transparent border border-[var(--color-theme-1))] shadow appearance-none focus:outline-none focus:shadow-outline text-bg"
           />
         </div>
-        <div class="flex flex-row">
-          <div class="flex-1">
-            <input
-              type="checkbox"
-              name="rememberMe"
-              class="appearance-none border-2 border-[var(--color-theme-1))] w-[15px] h-[15px] checkbox"
-            />
-            <label for=""> Remember me</label>
-          </div>
-          <a href="forgot" class="textGradient">Forgot Password</a>
-        </div>
         <button
           type="submit"
           class="bg-[var(--color-theme-1)] gradientBackground font-medium rounded-lg w-full my-6 py-4 text-center"
         >
-          Sign In
+          Sign Up
         </button>
-      </div>
-      <div class="flex">
-        <p class="mx-auto">
-          Don't have an account? <a href="register" class="textGradient">
-            Sign Up</a
-          >
-        </p>
       </div>
     </form>
   </div>
