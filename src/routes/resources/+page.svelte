a0a067e2c51378ca4cb67acb5b18ef893e0e8fd6
jiaxuan10 <limjiaxuan1357@gmail.com>
2024-04-10 16:29:56 +0800

    add resourcesid page
    add dynamic_route (resources page)

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
+  import { onMount } from 'svelte';
   import ResourceItem from "$lib/components/ResourcesItem.svelte";
+  import { supabase } from "../../main";
+
+  let resources: any[] = [];
+
+  onMount(async () => {
+    const { data, error } = await supabase.from('resources').select('*');
+    if (error) {
+      console.error('Error fetching resources:', error.message);
+    } else {
+      resources = data;
+    }
+  });
 </script>
 
 <svelte:head>
@@ -12,39 +25,18 @@
   </div>
 
   <div class="flex flex-col gap-y-4">
-    <ResourceItem
-      itemThumbnail="https://media.licdn.com/dms/image/D5612AQHNW3EKA-Fdyg/article-cover_image-shrink_720_1280/0/1682684162015?e=2147483647&v=beta&t=bR7zJ3dYWhkgs7DGotLo1HPAdzKUJOFVzmvYeLgW83A"
-      itemTitle="Mastering Money: A Comprehensive Guide to Financial Literacy"
-      itemTags={[
-        "#Crypto",
-        "#Bank",
-        "#2024",
-        "#Financial",
-        "#Money",
-        "#Guide",
-        "#Beginner",
-        "#Invest",
-        "#Finance",
-      ]}
-      views={651394}
-      likes={366545}
-      comments={56}
-    />
-    <ResourceItem
-      itemThumbnail="https://image.cnbcfm.com/api/v1/image/106826305-1624383819971-106826305-1611074293047-gettyimages-1295325607-_e6w9660_2021010863111059.jpg?v=1689789553&w=929&h=523&vtcrop=y"
-      itemTitle="Bitcoin Price Jumps 10% But Can Pump BTC Back To $70K?"
-      itemTags={["#BTC", "#bitcoin", "#2024"]}
-      views={651}
-      likes={365}
-      comments={6}
-    />
-    <ResourceItem
-      itemThumbnail="https://cdn.corporatefinanceinstitute.com/assets/financial-literacy1.jpeg"
-      itemTitle="Money Matters 101: Essential Financial Education for All"
-      itemTags={["#Money", "#Education", "#Beginner"]}
-      views={65}
-      likes={36545}
-      comments={6}
-    />
+    {#each resources as resource}
+    <a href="resources/{resource.id}">
+      <ResourceItem
+      itemThumbnail = {resource.itemThumbnail}
+      itemTitle= {resource.itemTitle}
+      itemTags= {resource.itemTags}
+      views={resource.views}
+      likes={resource.likes}
+      comments={resource.comments}
+      author = {resource.author}
+      />
+    </a>
+    {/each}
   </div>
 </div>
